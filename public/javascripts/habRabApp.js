7021964ea4ab310f42d6296bac1bbf06fbfa6fa8
Riley Todd <rileyatodd@gmail.com>
2015-05-14 01:41:09 -0400

    Added some tests and fixed up recordReinforcement as a result of failed tests

--- public/javascripts/habRabApp.js
+++ public/javascripts/habRabApp.js
@@ -1,3 +1,4 @@
+'use strict';
 var HABRAB = (function() {
 
   var retObj = {};
@@ -11,7 +12,7 @@ var HABRAB = (function() {
           habitElement = habitList.find('tbody').children().first();
         habitList.detach();
         //Fill in and uncollapse the habit elements that were rendered server-side
-        for (habit in user.habits) {
+        for (var habit in user.habits) {
           if (typeof habit !== 'function' && user.habits.hasOwnProperty(habit)) {
             habitElement.find('.habitName').text(habit);
             habitElement.removeClass('collapse');
@@ -20,20 +21,23 @@ var HABRAB = (function() {
         }
         parent.append(habitList);
       });
-  }
+  };
   retObj.populateHabitList = populateHabitList;
 
-  //Not sure how to factor this behavior yet, may be better to have this handled by something else...
-  var addHabit = function(user, habit) {
-    //create a new habit object and store it in the database
-  };
+  // //Not sure how to factor this behavior yet, may be better to have this handled by something else...
+  // var addHabit = function(user, habit) {
+  //   //create a new habit object and store it in the database
+  // };
 
   //Habit constructor, defines private variables via closure and returns a habit object with
   //public methods for accessing the private variables
   var newHabit = function(spec) {
     var self = {},
-      user = spec.user || 'Test User', //----CURRENTLY DEFAULTS TO TEST USER----
-      name = spec.name || 'Test Habit', //----CURRENTLY DEFAULTS TO TEST HABIT----
+      spec = spec || {},
+      user = spec.user, 
+      name = spec.name || 'Unnamed Habit',
+      frequency = spec.frequency || 1,
+      period = spec.period || 'day',
       habitRecord = []; //An array that stores the number of times the habit was reincforced for the last min(record.length, 30) days
 
     var getHabitRecord = function() {
@@ -41,31 +45,60 @@ var HABRAB = (function() {
     };
     self.getHabitRecord = getHabitRecord;
 
+    var getName = function() {
+      return name;
+    };
+    self.getName = getName;
+
+    var getUser = function() {
+      return user;
+    };
+    self.getUser = getUser;
+
+    var getFrequency = function() {
+      return frequency;
+    };
+    self.getFrequency = getFrequency;
+
+    var getPeriod = function() {
+      return period;
+    };
+    self.getPeriod = getPeriod;
+
     //Records how many times you reinforced a habit in a day
-    var recordReinforcement = function(times, daysAgo) {
+    var recordReinforcement = function(times, periodsAgo) {
       times = times || 1;
-      daysAgo = daysAgo || 0;
+      periodsAgo = periodsAgo || 0;
 
       //Record the number of times reinforced for the proper day
-      habitRecord[habitRecord.length - daysAgo] = times;
+      var index = habitRecord.length - periodsAgo - 1;
+      while (index < 0) {
+        habitRecord.unshift(0);
+        index += 1;
+      }
+      habitRecord[index] = habitRecord[index] ? habitRecord[index] + times : times;
 
       //Truncate the habitRecord to the most recent 30 days
       if (habitRecord.length > 30) {
-        habitRecord.splice(1,30);
+        habitRecord.splice(habitRecord.length - 30, 30);
       }
-    }
+    };
     self.recordReinforcement = recordReinforcement;
 
+    //Pushes current state to database via $.post
     var persistCurrentState = function() {
-      $.post('/users/' + user + '/habits/' + name, habitRecord)
-        .done(function(data){
+      if (!user) {
+        throw new Error('Undefined User');
+      }
+      $.post('/users/' + user + '/habits/' + name, {habitRecord: habitRecord, frequency: frequency, period: period})
+        .done(function(){
           populateHabitList(user);
         });
-    }
+    };
     self.persistCurrentState = persistCurrentState;
     
     return self;
-  }
+  };
   retObj.newHabit = newHabit;
 
   return retObj;
